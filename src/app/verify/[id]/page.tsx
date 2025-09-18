39e246043513c1390ed15e11956cbba30024ab20
lbachannel <anlb19749@gmail.com>
2025-09-18 15:55:29 +0700

    fix bug verify page #1

--- src/app/verify/[id]/page.tsx
+++ src/app/verify/[id]/page.tsx
@@ -1,6 +1,19 @@
 import VerifyComponent from "@/components/auth/VerifyComponent";
+import { Metadata } from 'next';
 
-const VerifyPage = ({ params }: { params: { id: string } }) => {
+interface VerifyPageProps {
+    params: {
+        id: string;
+    };
+}
+
+export const generateMetadata = ({ params }: VerifyPageProps): Metadata => {
+    return {
+        title: `Verify ${params.id}`,
+    };
+};
+
+const VerifyPage = ({ params }: VerifyPageProps) => {
     const { id } = params;
     return (
         <VerifyComponent id={id} />
