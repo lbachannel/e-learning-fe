d672b62e6b16a4014f6b936a7ed5cd07bda6ab75
lbachannel <anlb19749@gmail.com>
2025-09-18 13:42:28 +0700

    fix Unexpected token

--- src/components/ui/UserProfile.tsx
+++ src/components/ui/UserProfile.tsx
@@ -4,15 +4,9 @@ import { signOut } from "next-auth/react";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
-type TSession = {
-    session: Session | null
-}
-
-const UserProfile = (props: TSession) => {
+const UserProfile = ({ session }: { session: Session | null }) => {
     const [isOpen, setIsOpen] = useState(false);
     const username = useRef<HTMLSpanElement | null>(null);
-    const { session } = props;
-
 
     const handleClick = () => {
         setIsOpen(!isOpen);
