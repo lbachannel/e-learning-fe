a769ad1dfabee5d9a7ca486cec57f498198491a2
lbachannel <anlb19749@gmail.com>
2025-09-18 17:23:04 +0700

    test 5:22

--- src/app/verify/[id]/page.tsx
+++ src/app/verify/[id]/page.tsx
@@ -1,23 +1,43 @@
-import VerifyComponent from "@/components/auth/VerifyComponent";
+// import VerifyComponent from "@/components/auth/VerifyComponent";
+// import { Metadata } from 'next';
+
+// interface VerifyPageProps {
+//     params: {
+//         id: string;
+//     };
+// }
+
+// export const generateMetadata = ({ params }: VerifyPageProps): Metadata => {
+//     return {
+//         title: `Verify ${params.id}`,
+//     };
+// };
+
+// const VerifyPage = ({ params }: VerifyPageProps) => {
+//     const { id } = params;
+//     return (
+//         <VerifyComponent id={id} />
+//     )
+// }
+
+// export default VerifyPage;
+
+
+// app/verify/[id]/page.tsx
+import dynamic from 'next/dynamic';
 import { Metadata } from 'next';
 
-interface VerifyPageProps {
-    params: {
-        id: string;
-    };
-}
+const TestComponent = dynamic(() => import('@/components/auth/VerifyComponent'), {
+  ssr: false, // Tắt Server-Side Rendering
+});
 
-export const generateMetadata = ({ params }: VerifyPageProps): Metadata => {
-    return {
-        title: `Verify ${params.id}`,
-    };
-};
+interface VerifyPageProps {
+  params: { id: string };
+}
 
 const VerifyPage = ({ params }: VerifyPageProps) => {
-    const { id } = params;
-    return (
-        <VerifyComponent id={id} />
-    )
-}
+  const { id } = params;
+  return <TestComponent />;
+};
 
 export default VerifyPage;
